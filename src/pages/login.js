a03d1df966cbbb59561071fc332e45b73a6291c2
Markos <markosmk@me.com>
2022-04-08 14:44:02 -0300

    update: logic to auth implemented

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,22 +1,35 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { authUser } from '../services/auth';
 
 import Header from '../components/layout/Header';
-import warning from '../assets/icons/warning.svg';
+import InputForm from '../components/ui/InputForm';
 
+// TODO: pasar a formik
 export default function Login() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setSubmitting(true);
     authUser({ email, password }, 'login')
       .then((resp) => resp.json())
       .then((data) => {
+        setSubmitting(false);
+        if (data?.status === 400) {
+          setErrors(data.message || data.errors);
+          return;
+        }
+        setErrors(false);
         console.log(data);
+        navigate('/');
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => {});
   };
 
   return (
@@ -27,36 +40,30 @@ export default function Login() {
       </h2>
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col max-w-xs w-full mx-auto space-y-8 mt-12 md:mt-24"
+        className="flex flex-col max-w-xs w-full mx-auto space-y-8 md:space-y-12 mt-12 md:mt-24"
       >
-        <input
-          type="email"
+        <InputForm
           name="email"
-          value={email}
           placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full px-4 py-3 h-[45px] font-normal rounded-2xl md:rounded-lg border border-[#887675] focus:outline-none focus:ring-2 focus:ring-[#E063A3] focus:border-[#E063A3]"
+          value={email}
+          setValue={setEmail}
+          errors={errors}
+          required
         />
-
-        <div>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Contraseña"
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-4 py-3 h-[45px] font-normal rounded-2xl md:rounded-lg border border-[#F0281D] focus:outline-none focus:ring-2 focus:ring-[#E063A3] focus:border-[#E063A3]"
-          />
-          <p className="ml-2 mt-1 text-xs font-light text-[#F0281D] flex items-center gap-1">
-            <img src={warning} alt="incorrecto" />
-            <span>Contraseña Incorrecta</span>
-          </p>
-        </div>
+        <InputForm
+          name="password"
+          placeholder="Contraseña"
+          value={password}
+          required
+          setValue={setPassword}
+          errors={errors}
+        />
 
         <div className="inline-flex justify-center">
           <button
+            disabled={isSubmitting}
             type="submit"
-            className="w-full md:w-auto px-8 py-3 h-[50px] text-lg font-semibold text-white uppercase rounded-2xl md:rounded-[20px] md:py-1 md:h-[40px] bg-[#E063A3] hover:opacity-75 active:scale-95 transition-all"
+            className="w-full md:w-auto px-8 py-3 h-[50px] text-lg font-semibold text-white uppercase rounded-2xl md:rounded-[20px] md:py-1 md:h-[40px] bg-[#E063A3] hover:opacity-75 active:scale-95 transition-all disabled:opacity-50 disabled:pointer-events-none select-none"
           >
             Iniciar Sesión
           </button>
