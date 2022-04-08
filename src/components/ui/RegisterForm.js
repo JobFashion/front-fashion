ef7ee9e038d1ac52fdcb21255520bf93c1678c46
Markos <markosmk@me.com>
2022-04-08 14:44:59 -0300

    add: funcional components for input fields

--- src/components/ui/RegisterForm.js
+++ src/components/ui/RegisterForm.js
@@ -1,43 +1,41 @@
 import { useState } from 'react';
-import { authUser } from '../../services/auth';
-import warning from '../../assets/icons/warning.svg';
+import { useNavigate } from 'react-router-dom';
 
-function Input({ name, placeholder, value, setValue }) {
-  return (
-    <div className="relative">
-      <label forHtml={name} className="hidden absolute -top-5 left-2 text-xs md:block">
-        {placeholder}
-      </label>
-      <input
-        type={name === 'email' ? 'email' : 'text'}
-        name={name}
-        id={name}
-        value={value}
-        placeholder={placeholder}
-        onChange={(e) => setValue(e.target.value)}
-        className="w-full px-4 py-3 h-[45px] font-normal rounded-2xl md:rounded-lg border border-[#887675] focus:outline-none focus:ring-2 focus:ring-[#E063A3] focus:border-[#E063A3]"
-      />
-      <p className="absolute -bottom-[18px] md:-bottom-5 ml-2 text-[10px] font-light text-[#F0281D] flex items-center gap-1">
-        <img src={warning} alt="incorrecto" className="inline-flex md:hidden" />
-        <span>Este campo es obligatorio para el registro</span>
-      </p>
-    </div>
-  );
-}
+import { authUser } from '../../services/auth';
+import InputForm from './InputForm';
 
 function RegisterForm() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [birthDate, setBirthDate] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [repassword, setRepassword] = useState('');
+
+  const [errors, setErrors] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    authUser({ email, password }, 'register')
+    setSubmitting(true);
+    if (password !== repassword) return;
+    authUser({ name, surname, birthDate, email, password }, 'register')
       .then((resp) => resp.json())
       .then((data) => {
+        setSubmitting(false);
+        if (data?.status === 400) {
+          setErrors(data.message || data.errors);
+          return;
+        }
+        setErrors(false);
         console.log(data);
+        navigate('/');
       })
       .catch((err) => console.log(err));
   };
+  // console.log(errors);
 
   return (
     <form
@@ -45,17 +43,58 @@ function RegisterForm() {
       className="flex flex-col max-w-xs md:max-w-xl w-full mx-auto space-y-8 mt-8 md:space-y-12 md:mt-24"
     >
       <div className="grid grid-cols-1 md:grid-cols-2 gap-y-7 gap-x-10 md:gap-y-12">
-        <Input name="name" placeholder="Nombre" value="" setValue />
-        <Input name="surname" placeholder="Apellido" value="" setValue />
-        <Input name="email" placeholder="Email" value="" setValue={setEmail} />
-        <Input name="birthDate" placeholder="Fecha de Nacimiento" value="" setValue />
-        <Input name="password" placeholder="Contraseña" value="" setValue={setPassword} />
-        <Input name="repassword" placeholder="Confirmar contraseña" value="" setValue />
+        <InputForm
+          name="name"
+          placeholder="Nombre"
+          value={name}
+          setValue={setName}
+          errors={errors}
+        />
+        <InputForm
+          name="surname"
+          placeholder="Apellido"
+          value={surname}
+          setValue={setSurname}
+          errors={errors}
+        />
+        <InputForm
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          setValue={setEmail}
+          errors={errors}
+        />
+        <InputForm
+          name="birthDate"
+          type="date"
+          placeholder="Fecha de Nacimiento"
+          value={birthDate}
+          setValue={setBirthDate}
+          errors={errors}
+        />
+        <InputForm
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          value={password}
+          setValue={setPassword}
+          errors={errors}
+        />
+        <InputForm
+          type="password"
+          name="repassword"
+          placeholder="Confirmar contraseña"
+          value={repassword}
+          setValue={setRepassword}
+          errors={password !== repassword ? 'El password debe coincidir' : ''}
+        />
       </div>
       <div className="inline-flex justify-center">
         <button
+          disabled={isSubmitting}
           type="submit"
-          className="w-full md:w-auto px-8 py-3 h-[50px] text-lg font-semibold text-white uppercase rounded-2xl md:rounded-[20px] md:py-1 md:h-[40px] bg-[#E063A3] hover:opacity-75 active:scale-95 transition-all"
+          className="w-full md:w-auto px-8 py-3 h-[50px] text-lg font-semibold text-white uppercase rounded-2xl md:rounded-[20px] md:py-1 md:h-[40px] bg-[#E063A3] hover:opacity-75 active:scale-95 transition-all disabled:opacity-50 disabled:pointer-events-none select-none"
         >
           Crear Cuenta
         </button>
