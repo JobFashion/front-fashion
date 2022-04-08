18c5952a91a69c9576a692ceb5ee6c1d91cadcd4
Markos <markosmk@me.com>
2022-04-07 23:44:18 -0300

    update: added new routes and layout public

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import Landing from './pages/landing';
 import Login from './pages/login';
 import Recovery from './pages/recovery';
 import Register from './pages/register';
+import Layout from './components/layout';
+import RegisterSuccess from './pages/RegisterSuccess';
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/recovery" element={<Recovery />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recovery" element={<Recovery />} />
+        <Route path="/success" element={<RegisterSuccess />} />
+      </Route>
     </Routes>
   );
 }
