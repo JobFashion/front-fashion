ca3857b6c53461c102fe2f69a586520c639dd644
Markos <markosmk@me.com>
2022-04-09 23:50:35 -0300

    add: control for pages

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from './Home';
+export { default as Profile } from './Profile';
+export { default as RegisterSuccess } from './RegisterSuccess';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as Recovery } from './Recovery';
+export { default as Landing } from './landing';
