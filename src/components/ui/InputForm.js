2837fc12a91eadb09dd5d27a142c4cbcc1b877d0
Markos <markosmk@me.com>
2022-04-09 23:48:05 -0300

    update: component function for formik

--- src/components/ui/InputForm.js
+++ src/components/ui/InputForm.js
@@ -1,4 +1,6 @@
 import warning from '../../assets/icons/warning.svg';
+import ok from '../../assets/icons/ok.svg';
+import not_ok from '../../assets/icons/not_ok.svg';
 
 function InputForm({
   name,
@@ -8,6 +10,7 @@ function InputForm({
   required,
   errors,
   formikInput = false,
+  formikCheck,
 }) {
   const type = {
     email: 'email',
@@ -17,23 +20,49 @@ function InputForm({
   };
 
   if (formikInput) {
+    const iconRePass = () => {
+      if (
+        !formikInput.errors.repassword &&
+        formikInput.values.repassword &&
+        formikInput.values.password
+      ) {
+        return ok;
+      } else if (
+        formikInput.errors.repassword &&
+        formikInput.values.repassword &&
+        formikInput.values.password
+      ) {
+        return not_ok;
+      } else {
+        return null;
+      }
+    };
+
     return (
       <div className="relative">
         <label htmlFor={name} className="hidden absolute -top-5 left-2 text-xs md:block">
           {placeholder}
         </label>
-        <input
-          id={name}
-          type={type[name] ? type[name] : 'text'}
-          required={required}
-          placeholder={placeholder}
-          {...formikInput.getFieldProps(name)}
-          className={`w-full px-4 py-3 h-[45px] font-normal rounded-2xl md:rounded-lg border ${
-            formikInput.touched[name] && formikInput.errors[name]
-              ? 'border-[#F0281D]'
-              : 'border-[#887675]'
-          } focus:outline-none focus:ring-2  focus:ring-[#E063A3] focus:border-[#E063A3]`}
-        />
+
+        <div className="relative">
+          <input
+            id={name}
+            type={type[name] ? type[name] : 'text'}
+            required={required}
+            placeholder={placeholder}
+            {...formikInput.getFieldProps(name)}
+            className={`w-full px-4 py-3 h-[45px] font-normal rounded-2xl md:rounded-lg border ${
+              formikInput.touched[name] && formikInput.errors[name]
+                ? 'border-[#F0281D]'
+                : 'border-[#887675]'
+            } focus:outline-none focus:ring-2  focus:ring-[#E063A3] focus:border-[#E063A3] placeholder:text-slate-400`}
+          />
+          {formikCheck && iconRePass() && (
+            <div className="absolute top-4 right-3">
+              <img src={iconRePass()} alt="todo not ok" />
+            </div>
+          )}
+        </div>
 
         {formikInput.touched[name] && formikInput.errors[name] ? (
           <p className="absolute -bottom-[18px] md:-bottom-5 left-2 text-[10px] font-light text-[#F0281D] flex items-center gap-1 w-full">
