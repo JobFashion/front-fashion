689b2fc12c3f1fdf157aa7cbfbeeebcddc63608f
Markos <markosmk@me.com>
2022-04-09 23:59:20 -0300

    fixed: update lowercase first letter in name files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export { default as Home } from './Home';
 export { default as Profile } from './Profile';
 export { default as RegisterSuccess } from './RegisterSuccess';
-export { default as Login } from './Login';
-export { default as Register } from './Register';
-export { default as Recovery } from './Recovery';
+export { default as Login } from './login';
+export { default as Register } from './register';
+export { default as Recovery } from './recovery';
 export { default as Landing } from './landing';
