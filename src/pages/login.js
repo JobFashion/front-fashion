bda3075b15f033930a14a946008a6841814140f3
Markos <markosmk@me.com>
2022-04-09 23:51:05 -0300

    update: logic components for work with backend

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,36 +1,63 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useFormik } from 'formik';
 import { Link, useNavigate } from 'react-router-dom';
-import { authUser } from '../services/auth';
+import { useSelector, useDispatch } from 'react-redux';
+import { login, reset } from '../store/auth/authSlice';
 
 import Header from '../components/layout/Header';
 import InputForm from '../components/ui/InputForm';
+import Spinner from '../components/icons/Spinner';
 
-// TODO: pasar a formik
+const validate = (values) => {
+  const errors = {};
+  // similares
+  const forEmpty = 'Necesitamos que completes este campo';
+
+  if (!values.email) {
+    errors.email = forEmpty;
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Debes ingresar un email válido';
+  }
+
+  if (!values.password) {
+    errors.password = forEmpty;
+  }
+
+  return errors;
+};
+
 export default function Login() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState('');
-  const [isSubmitting, setSubmitting] = useState(false);
+  const dispatch = useDispatch();
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-    authUser({ email, password }, 'login')
-      .then((resp) => resp.json())
-      .then((data) => {
-        setSubmitting(false);
-        if (data?.status === 400) {
-          setErrors(data.message || data.errors);
-          return;
-        }
-        setErrors(false);
-        console.log(data);
-        navigate('/');
-      })
-      .catch((err) => console.log(err))
-      .finally(() => {});
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate,
+    onSubmit: (values) => {
+      dispatch(login(values));
+    },
+  });
+
+  useEffect(() => {
+    if (isError) {
+      if (!Array.isArray(isError)) {
+        formik.setFieldError('email', message);
+        formik.setFieldError('password', message);
+      }
+    }
+    if (isSuccess || user) {
+      navigate('/home');
+    }
+
+    dispatch(reset());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   return (
     <>
@@ -39,46 +66,39 @@ export default function Login() {
         Iniciar sesión
       </h2>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={formik.handleSubmit}
         className="flex flex-col max-w-xs w-full mx-auto space-y-8 md:space-y-12 mt-12 md:mt-24"
       >
-        <InputForm
-          name="email"
-          placeholder="Email"
-          value={email}
-          setValue={setEmail}
-          errors={errors}
-          required
-        />
-        <InputForm
-          name="password"
-          placeholder="Contraseña"
-          value={password}
-          required
-          setValue={setPassword}
-          errors={errors}
-        />
+        <InputForm name="email" placeholder="Email" formikInput={formik} />
+        <InputForm name="password" placeholder="Contraseña" formikInput={formik} />
 
         <div className="inline-flex justify-center">
           <button
-            disabled={isSubmitting}
+            disabled={!formik.isValid || isLoading}
             type="submit"
             className="w-full md:w-auto px-8 py-3 h-[50px] text-lg font-semibold text-white uppercase rounded-2xl md:rounded-[20px] md:py-1 md:h-[40px] bg-[#E063A3] hover:opacity-75 active:scale-95 transition-all disabled:opacity-50 disabled:pointer-events-none select-none"
           >
-            Iniciar Sesión
+            <Spinner disabled={isLoading} />
+            <span>Iniciar Sesión</span>
           </button>
         </div>
       </form>
       <div className="text-center mt-6 md:mt-8 font-medium text-xs md:text-sm space-y-3 md:space-y-5">
         <p>
           ¿Olvidaste tu contraseña?
-          <Link to="/recovery" className="text-[#E063A3] md:font-semibold ml-1">
+          <Link
+            to="/recovery"
+            className="text-[#E063A3] md:font-semibold ml-1 hover:opacity-75 transition-opacity"
+          >
             Ingresá aquí.
           </Link>
         </p>
         <p>
           ¿No tienes una cuenta?
-          <Link to="/register" className="text-[#E063A3] md:font-semibold ml-1">
+          <Link
+            to="/register"
+            className="text-[#E063A3] md:font-semibold ml-1 hover:opacity-75 transition-opacity"
+          >
             Registrate aquí.
           </Link>
         </p>
