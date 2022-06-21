eb9c640caf77f68de09f4050e518d6d132842be1
Markos <markosmk@me.com>
2022-06-20 22:44:29 -0300

    add: missing files

---
+++ src/services/dataMock.js
@@ -0,0 +1,34 @@
+export const users = [
+  {
+    id: '62810a41dd1e78fe0ceb787b',
+    name: 'Lorena Gonz',
+    email: 'firulais@gmail.com',
+    birthDate: '1991-10-23T00:00:00.000Z',
+    avatar: 'https://media.canalnet.tv/2021/08/lali-espo-974x550.jpg',
+    followers: ['Maca'],
+  },
+  {
+    id: '62810a41dd1e78fe0ceb7872',
+    name: 'Julieta Volz',
+    email: 'firulais@gmail.com',
+    birthDate: '1991-10-23T00:00:00.000Z',
+    avatar: 'https://i.pinimg.com/280x280_RS/25/3b/39/253b39b410cdd43462f3497b4ff5e774.jpg',
+    followers: ['Facundo', 'Julian', 'Anibal'],
+  },
+  {
+    id: '62810a41dd1e78fe0ceb7875',
+    name: 'Dario Betthes',
+    email: 'firulais@gmail.com',
+    birthDate: '1991-10-23T00:00:00.000Z',
+    avatar: 'https://pbs.twimg.com/profile_images/1450453989271691264/4ZEMrQJG_400x400.jpg',
+    followers: [],
+  },
+  {
+    id: '62810a41dd1e78fe0ceb7879',
+    name: 'Veronica Duiolzs',
+    email: 'firulais@gmail.com',
+    birthDate: '1991-10-23T00:00:00.000Z',
+    avatar: 'https://i.pinimg.com/280x280_RS/f0/97/a3/f097a32d2afb1c6f8b52a9cc1d98e6c4.jpg',
+    followers: ['Dario', 'Julian', 'Anibal', 'Maca', 'Luz'],
+  },
+];
