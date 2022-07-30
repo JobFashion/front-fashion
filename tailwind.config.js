bfa8001eb368b051b0aed49f1cf5f0971811cb0f
Markos <markosmk@me.com>
2022-07-30 20:24:10 -0300

    update: colors tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
+const colors = require('tailwindcss/colors');
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      slate: colors.slate,
+      white: colors.white,
+      pink: colors.pink,
+      main: {
+        rosa: '#E063A3',
+        amarillo: '#FFC911',
+      },
+      secondary: {
+        rosa: '#F49CC0',
+        marron: '#F4D197',
+        negro: '#1B1616',
+        sombra: '#887675',
+        'rosa-puro': '#F3EAE8',
+        fondo: '#F2F2F2',
+        'texto-ayuda': '#939393',
+        comentario: '#F5C3D7', // => rosa 50% of opacity
+        bordo: '#860B3D',
+        gris: '#656565',
+        'gris-claro': '#D9D8D8',
+      },
+      third: {
+        check: '#22A900',
+        rojo: '#F0281D',
+        link: '#2D33C8',
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
